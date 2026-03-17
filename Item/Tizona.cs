a38cd84478f66ea2429457d3b0717922feb3d4a9
unknown <therealmr.polo1@gmail.com>
2026-03-16 17:10:52 -0700

    got working

--- Item/Tizona.cs
+++ Item/Tizona.cs
@@ -1,41 +1,24 @@
-public void UseItemEffect(Player player, Rectangle rectangle)   //notes will be written to assist you further kingicarus
+public void UseItemEffect(Player player, Rectangle rectangle)
 {
     int dust = Dust.NewDust(
-    new Vector2((float) rectangle.X, (float) rectangle.Y),      //position
-    rectangle.Width,                                            //box width
-    rectangle.Height,                                           //box height
-    3,                                                          //type
-    (player.velocity.X * 0.2f) + (player.direction * 3),        //X velocity
-    player.velocity.Y * 0.2f,                                   //Y velocity
-    100,                                                        //alpha
-    new Color(),                                                //Color override
-    1.8f                                                        //scale
+        new Vector2(rectangle.X, rectangle.Y),
+        rectangle.Width,
+        rectangle.Height,
+        6, // flame dust
+        player.velocity.X * 0.2f + player.direction * 3,
+        player.velocity.Y * 0.2f,
+        100,
+        default(Color),
+        1.8f
     );
-    Main.dust[dust].noGravity = true;
 
-    int dust2 = Dust.NewDust(
-    new Vector2((float) rectangle.X, (float) rectangle.Y),      //position
-    rectangle.Width,                                            //box width
-    rectangle.Height,                                           //box height
-    6,                                                          //type
-    (player.velocity.X * 0.2f) + (player.direction * 3),        //X velocity
-    player.velocity.Y * 0.2f,                                   //Y velocity
-    100,                                                        //alpha
-    new Color(),                                                //Color override
-    1.8f                                                        //scale
-    );
-    Main.dust[dust2].noGravity = true;
+    Main.dust[dust].noGravity = true;
 }
+
 public void DamageNPC(Player player, NPC npc, ref int damage, ref float knockback)
 {
-	if (Main.rand.Next(5) == 0)
-	{
-        // Add the buff 
-		npc.AddBuff(20, 420);
-	}
-	if (Main.rand.Next(5) == 0)
-	{
-        // Add the buff 
-		npc.AddBuff(24, 420);
-	}
+    if (Main.rand.Next(5) == 0)
+    {
+        npc.AddBuff(30, 420); // Bleeding
+    }
 }
