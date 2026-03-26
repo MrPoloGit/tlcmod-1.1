6dd38beb15ff755c15e0bee757ba3d52211b10da
unknown <therealmr.polo1@gmail.com>
2026-03-26 13:41:06 -0700

    Added Vampire Miner and Servant of Ocram

--- NPC/Shadow Slime.cs
+++ NPC/Shadow Slime.cs
@@ -1,22 +1,21 @@
-public bool SpawnNPC(int x, int y, int playerID)
-{
-    Player player = Main.player[playerID];
+// public bool SpawnNPC(int x, int y, int playerID)
+// {
+//     Player player = Main.player[playerID];
 
-    if (player.zoneEvil) // corruption biome
-    {
-        if (y < Main.rockLayer - 10 &&
-            y > (int)(Main.topWorld + 100f) &&
-            !Main.dayTime &&
-            !player.zoneMeteor &&
-            Main.rand.Next(40) == 0)
-        {
-            return true;
-        }
-    }
+//     if (player.zoneEvil) // corruption biome
+//     {
+//         if (y < Main.rockLayer - 10 &&
+//             y > (int)(Main.topWorld + 100f) &&
+//             !Main.dayTime &&
+//             !Main.player[playerID].zoneMeteor &&
+//             Main.rand.Next(40) == 0)
+//         {
+//             return true;
+//         }
+//     }
 
-    return false;
-}
-
+//     return false;
+// }
 
 public void NPCLoot()
 {
@@ -24,5 +23,3 @@ public void NPCLoot()
 	Dust.NewDust(npc.position, npc.width, npc.height, 4, 0.2f, 0.2f, 100, default(Color), 1f);
 	Dust.NewDust(npc.position, npc.width, npc.height, 4, 0.2f, 0.2f, 100, default(Color), 1f);
 }
-
-
